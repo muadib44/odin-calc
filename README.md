369fabd2c0c4379ccd02c54e208095836a5c575f
muadib44 <kartikeyajangid54@gmail.com>
2026-06-02 11:46:19 +0000

    Create all calculating function and a function which lets you use them

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odin-calc
+This is the final foundations project. Using Objects and all the previous concepts, we are creating this calculator.
---
+++ script.js
@@ -0,0 +1,37 @@
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    if (num2 === 0) {
+        return 'Error: Division by zero';
+    }
+    return num1 / num2;
+}
+
+function operate (operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'Error: Invalid operator';
+    }
+}
